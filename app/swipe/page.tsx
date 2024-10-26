30b8987b8843fc4291dfebd399538b54ab0a2cbe
jcl0251 <josephcleal529@gmail.com>
2024-10-26 18:07:27 -0500

    Add page

--- app/swipe/page.tsx
+++ app/swipe/page.tsx
@@ -7,13 +7,7 @@ import { Certificate } from "crypto";
 const swipe = () => {
   return (
     <div style={{
-      height: '100vh',
-      width: '100vw',
-      overflow: 'hidden',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center'
+      
     }}> 
         <h1>Swipe</h1>
         <TinderCards /> {/* Displaying the TinderCards component */}
