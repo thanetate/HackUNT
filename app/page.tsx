ea3230817f47b20011e75f65659d43a8e35bface
Sheahen, Kathryn <kathrynsheahen@my.unt.edu>
2024-10-26 15:09:37 -0500

    home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image";import styles from "./page.modeul.css";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
-      <h1>Home</h1>
+      {/* Header will need logo, links, log in button, and nav */}
+      <Header /> 
+        {/* Landing Area */}
+          <div className="landing">
+          <h1 className="landing-desc">Where Monsters Find Their Matches</h1>
+          <img src="./pumpkin_mainhome_icon.png" alt="Monster Match Logo"/>
+          <button>Sign Up</button>
+        </div>
+        {/* Our Approach */}
+        {/* What Other Monsters are Saying / User Feedback */}
+        {/* Footer */}
+
     </main>
   );
 }
