f78e7791c2ba7dfc8ecc1aaa53f56d50d49b32e5
Tate, Thane <thanetate@my.unt.edu>
2024-10-26 17:27:08 -0500

    styles

---
+++ app/components/Navbar/page.tsx
@@ -0,0 +1,24 @@
+
+
+
+function Navbar () {    
+
+    return (
+        <>
+            <div className="nav-container">
+                <div className="title">Monster Match</div>    
+                <div className="nav-links-container">
+                    <button>Home</button>
+                    <div>|</div>
+                    <button>Swipe</button>
+                    <div>|</div>
+                    <button>Dashboard</button>
+                    <div>|</div>
+                </div>
+            </div>        
+
+        </>
+    );
+};
+
+export default Navbar;
