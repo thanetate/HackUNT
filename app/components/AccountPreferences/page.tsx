417a4c7d51257910f933de3c38260ac5d4ba1d35
Tate, Thane <thanetate@my.unt.edu>
2024-10-26 21:33:19 -0500

    dash

--- app/components/AccountPreferences/page.tsx
+++ app/components/AccountPreferences/page.tsx
@@ -1,8 +1,4 @@
-
-
 function AccountPreferences() {
-
-
 	return (
 		<>
 			<h1>Account Preferences</h1>
@@ -13,23 +9,22 @@ function AccountPreferences() {
 				// onChange={handleNameChange}
 			/>
 			<div className="middle-spooky-title">What kind of monster are you?</div>
-            <input
-        className="middle-select-box"
-		value={"Zombie"}
-        readOnly
-        // onChange={handleOptionChange}
-      />
-            <div className="middle-des-title">Description</div>
+			<input
+				className="middle-select-box"
+				value={"Zombie"}
+				readOnly
+				// onChange={handleOptionChange}
+			/>
+			<div className="middle-des-title">Description</div>
 			<textarea
 				className="middle-des-box"
 				// value={longdesc}
 				// onChange={handleLongDescChange}
 			></textarea>
 
-            <div className="main-profile-pic">
+			<div className="main-profile-pic">
 				{/* Calls avatar and bc avatar is set to a url we can just src the url */}
-				{/* {avatar && <img src={avatar} alt="User Avatar" />} */}
-                .
+				{/* {avatar && <img src={avatar} alt="User Avatar" />} */}.
 			</div>
 			<button className="middle-change-btn">
 				<img src="./img.svg" alt="picture icon" />
@@ -39,7 +34,6 @@ function AccountPreferences() {
 				<img src="./trash.svg" alt="trash icon" />
 				Remove
 			</button>
-
 		</>
 	);
 }
